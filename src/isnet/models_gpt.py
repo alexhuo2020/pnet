8b15865fab48d9499b60aaa35242353019c335c1
alexhuo2020 <136142213+alexhuo2020@users.noreply.github.com>
2024-03-05 22:03:04 -0500

    Update models_gpt.py

--- src/isnet/models_gpt.py
+++ src/isnet/models_gpt.py
@@ -281,7 +281,7 @@ class GPT(nn.Module):
         # Create AdamW optimizer and use the fused version if it is available
         fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
         use_fused = fused_available and device_type == 'cuda'
-        extra_args = dict(fused=True) if use_fused else dict()
+        extra_args = dict(fused=False) if use_fused else dict()
         # optimizer = torch.optim.RMSprop(optim_groups, lr=learning_rate)#, betas=betas, **extra_args)
         optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
         print(f"using fused AdamW: {use_fused}")
